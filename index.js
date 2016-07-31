49e2dd6309381ee2c0d1777d3349b0179670ba7e
Mason Long <masonlong92@gmail.com>
2016-07-31 16:32:52 -0700

    v1

---
+++ index.js
@@ -0,0 +1,38 @@
+var express = require('express');
+var app = express();
+var http = require('http');
+var https = require('https');
+var bodyParser = require("body-parser");
+
+app.get('/', function(req, res){
+  res.send('hello world');
+});
+
+app.get('/today', function(request, response){
+    var url = 'http://www.checkiday.com/api/3/?d=today';
+
+    http.get(url, function(res){
+    var body = '';
+
+    res.on('data', function(chunk){
+        body += chunk;
+    });
+
+    res.on('end', function(){
+        var holidayResponse = JSON.parse(body);
+        var holidayArray = [];
+        for(var i = 0; i < holidayResponse.holidays.length; i++){
+            var dayString = holidayResponse.holidays[i].name;
+            holidayArray.push(dayString);
+        };
+        console.log("Got a response: ", holidayResponse);
+        response.json(['text', holidayArray]);
+    });
+}).on('error', function(e){
+      console.log("Got an error: ", e);
+});
+});
+
+
+app.listen(4268);
+console.log('The app is running on port 4268');
